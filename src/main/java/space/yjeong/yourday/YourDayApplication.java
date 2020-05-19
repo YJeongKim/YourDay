10111513ddaba990f769f389306da8fe210a044e
YJeongKim <yjeong20s@naver.com>
2020-05-19 18:57:03 +0900

    init : Create project

---
+++ src/main/java/space/yjeong/yourday/YourDayApplication.java
@@ -0,0 +1,13 @@
+package space.yjeong.yourday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourDayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/space/yjeong/yourday/YourDayApplicationTests.java
@@ -0,0 +1,13 @@
+package space.yjeong.yourday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
